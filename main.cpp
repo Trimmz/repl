06e8ca7da075ba6f2dbdf12ae5beeb85dfc3c4c9
[GD] AsrielFighter AsrielFighter <90043875+Trimmz@users.noreply.github.com>
2021-10-16 19:11:25 +0000

    I fixed it

--- main.cpp
+++ main.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
-#include <vector>
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
 
+void dupeCheck(int (&arr)[100])
+{
+  for(int i =0; i<100; i++)
+  {
+    int toCheck = arr[i];
+
+    for(int j =0; j<i;j++)
+    {
+      if(toCheck == arr[j])
+      {
+        arr[i] = rand() % 150 + 1;
+
+        dupeCheck(arr);
+      }
+    }
+  }
+}
+
 int main() 
 {
-  vector <int> list;
+  int list[100];
   int number;
   srand(time(NULL));
-
-  for(int i; i<100; i++)
+  for(int i = 0; i<size(list); i++)
   {
+  
     number = rand() % 150 + 1;
-    for(int j; j<list.size(); j++)
+    
+    for(int j = 0; j<=i; j++)
     {
-      if(i == j)
-      {
-        continue;
-      }
-      while(number==list[i])
+     
+      while(number == list[j] || number == list[i])
       {
         number = rand() % 150 + 1;
       }
     }
-    list.push_back(number);
+    list[i] = number;
   }
+  dupeCheck(list);
   for(int i; i<100; i++)
   {
     cout<<list[i]<<endl;
